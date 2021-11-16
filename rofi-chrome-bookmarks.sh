4eef84ff3a5de72cc5927a6ade753af35b048c04
tylerbrandt <tbrandtsiue@gmail.com>
2021-11-16 11:52:57 -0500

    Added 'bookmarks' to the prompt

--- rofi-chrome-bookmarks.sh
+++ rofi-chrome-bookmarks.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-parse-chrome-bookmarks "$1" \
-  | rofi -i -dmenu \
+SELECTED=$(parse-chrome-bookmarks "$1")
+
+# doesn't work - it's like rofi doesn't wait for selection to happen
+# if [[ -z "$SELECTED" ]]; then
+#     echo 'No selection was made'
+#     exit 1
+# fi
+# echo "selected: $SELECTED"
+
+echo "$SELECTED" \
+  | rofi -i -dmenu -p 'bookmarks' \
   | awk '{print $2}' \
   | sed 's/"//g' \
   | xargs google-chrome
